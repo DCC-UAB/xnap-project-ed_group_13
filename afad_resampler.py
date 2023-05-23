a7f7edebb48e62c10798e930da1596989918d75d
berniksuni <83655413+berniksuni@users.noreply.github.com>
2023-05-23 17:47:29 +0200

    los resamples resampleaos
    aqui van

---
+++ afad_resampler.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+train = pd.read_csv('afad_train.csv')
+train.sample(n=10000,axis=0,random_state = 420).reset_index().to_csv('afad_train_sample.csv')
+
+test = pd.read_csv('afad_test.csv')
+test.sample(n=256,axis=0,random_state = 420).reset_index().to_csv('afad_test_sample.csv')
+
+valid = pd.read_csv('afad_valid.csv')
+valid.sample(n=256,axis=0,random_state = 420).reset_index().to_csv('afad_valid_sample.csv')
+
